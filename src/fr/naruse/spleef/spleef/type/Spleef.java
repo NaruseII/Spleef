93c49da65033917afcf4439f5a45e7df99939956
SoRadGaming <47128515+SoRadGaming@users.noreply.github.com>
2020-12-31 18:03:46 +1100

    Update Spleef.java

--- src/fr/naruse/spleef/spleef/type/Spleef.java
+++ src/fr/naruse/spleef/spleef/type/Spleef.java
@@ -432,6 +432,7 @@ public class Spleef extends BukkitRunnable implements Listener {
                 p.sendMessage(getFullName()+" "+pl.getMessageManager().get("playerWins", new String[]{"name"}, new String[]{p.getName()}));
             }
             playerInGame.remove(p);
+            SpleefPlayer spleefPlayer = pl.getSpleefPlayerRegistry().getSpleefPlayer(p);
             p.getInventory().clear();
             p.updateInventory();
             updateSigns();
