a56625b0a8dfefcc5324784036c15591ad170751
NaruseII <naruse.dev@gmail.com>
2021-05-11 22:53:43 +0200

    remove unused lines

--- src/fr/naruse/spleef/event/Listeners.java
+++ src/fr/naruse/spleef/event/Listeners.java
@@ -1,17 +1,11 @@
 package fr.naruse.spleef.event;
 
-import fr.naruse.spleef.api.SpleefBonusInitEvent;
 import fr.naruse.spleef.main.SpleefPlugin;
 import fr.naruse.spleef.player.SpleefPlayer;
-import fr.naruse.spleef.spleef.bonus.Bonus;
-import fr.naruse.spleef.spleef.bonus.BonusColored;
-import fr.naruse.spleef.spleef.bonus.BonusManager;
 import fr.naruse.spleef.spleef.type.Spleef;
-import fr.naruse.spleef.utils.BlockBuffer;
 import fr.naruse.spleef.utils.SpleefUpdater;
 import fr.naruse.spleef.utils.Utils;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
@@ -23,7 +17,6 @@ import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.util.Vector;
 
 import java.util.List;
 
@@ -129,33 +122,4 @@ public class Listeners implements Listener {
         }
     }
 
-    @EventHandler
-    public void spleefBonusInitEvent(SpleefBonusInitEvent e){
-        List<Class<? extends Bonus>> bonuses = e.getBonuses();
-        bonuses.add(BonusCustom.class);
-    }
-
-    public class BonusCustom extends BonusColored {
-
-        public BonusCustom(BonusManager bonusManager, Player p) {
-            super(bonusManager, p, "§aMy Awesome Bonus", 4, 5);
-        }
-
-        @Override
-        protected void onAction() {
-
-            BlockBuffer blockBuffer = new BlockBuffer(); // Basically a Set<Block>
-            for (int i = 0; i < 5; i++) { //Looping 5 times
-                for (Block block : Utils.getCircle(sheep.getLocation(), i)) { // Getting all blocks on a circle with i blocks of radius
-                    if(block.getType() == Material.SNOW_BLOCK || block.getType() == Material.TNT){ // Checking if we only have spleef's blocks
-                        blockBuffer.add(block); // Adding block in buffer
-                    }
-                }
-            }
-
-            runSync(() -> spleef.destroyBlock(p, blockBuffer)); // Destroying blocks in buffer
-            // Running on main Thread only  the for each to change block type
-            // The getCircle calculation doesn't need to be on main Thread as it will cause lag if it's called too often
-        }
-    }
 }
